7778b93da15a3a651e19d18ecc7b6c767cf31e78
Andrei Chupeev <cad.dsw.dwa@gmail.com>
2022-11-15 20:43:07 +0500

    add lab 7 task 2

--- lab_7/Task_2/Task_2/MainWindow.xaml.cs
+++ lab_7/Task_2/Task_2/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
 
 namespace Task_2
 {
@@ -12,9 +17,65 @@ namespace Task_2
             InitializeComponent();
         }
 
+        private string[] _lines;
+        private string _nameOfFile;
+        private string _longestWorld;
+        private List<string> _words;
         private void ChooseFile_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            ChooseFileWindow win = new ChooseFileWindow();
+            if (win.ShowDialog() == true)
+            {
+                if (win.ChosenFiel != "nothing")
+                {
+                    _nameOfFile = $"{win.ChosenFiel}";
+                    _lines = File.ReadAllLines($"Files/{win.ChosenFiel}");
+                    foreach (var line in _lines)
+                    {
+                        TopTextBox.Text += $"{line}{Environment.NewLine}";
+                    }
+                }
+            }
+        }
+
+        private void ShowData_OnClick(object sender, RoutedEventArgs e)
+        {
+            BottomTextBox.Text = "";
+            var nl = Environment.NewLine;
+            BottomTextBox.Text += $"Имя файла: {_nameOfFile}{nl}";
+            BottomTextBox.Text += $"Не пустых слов слово:{_lines.Where(x => x != "").Count()}{nl}";
+            _longestWorld = _lines.Where(x => x != "").First(x => x.Length == _lines.Max(xx => xx.Length));
+            var litWord = _lines.Where(x => x != "")
+                .First(x => x.Length == _lines.Where(xx => xx != "").Min(xx => xx.Length));
+            BottomTextBox.Text += $"Самое длинное слово:{_longestWorld}{nl}";
+            BottomTextBox.Text += $"Самое короткое солов:{litWord}{nl}";
+            var mid = _lines.Length / 2;
+            BottomTextBox.Text += $"Медианная строка:{_lines[mid]}{nl}";
+        }
+
+        private void ChangeInfo_OnClick(object sender, RoutedEventArgs e)
+        {
+            BottomTextBox.Text = "";
+            var nl = Environment.NewLine;
+            var pattern = @"\w+";
+            _words = new List<string>();
+            _lines.Where(x => x != "").ToList().ForEach(x =>
+            {
+                foreach (Match match in Regex.Matches(x, pattern))
+                {
+                    _words.Add(match.Value);
+                } 
+            });
+            BottomTextBox.Text += $"{_nameOfFile}{nl}";
+            BottomTextBox.Text += $"{_words.Count}{nl}";
+            _longestWorld = _words.First(x => x.Length == _words.Max(xx => xx.Length));
+            BottomTextBox.Text += $"{_words.Max(x => x.Length)}{nl}";
+            BottomTextBox.Text += $"{_longestWorld}{nl}";
+            for (var i = 1; i <= _longestWorld.Length; i++)
+            {
+                BottomTextBox.Text += $"{i} | {_words.Count(x => x.Length == i)}{nl}";
+            }
+
         }
     }
 }
