b0d0217a5daaebdd933bb1ac752a2b5e4a4dca3a
Andrei Chupeev <cad.dsw.dwa@gmail.com>
2022-11-15 15:12:53 +0500

    add task 2

---
+++ lab_7/Task_2/Task_2/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
