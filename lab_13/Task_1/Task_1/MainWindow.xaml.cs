88513a51a6008bae6b18067c39a1c09e4cc2418a
Andrei Chupeev <cad.dsw.dwa@gmail.com>
2022-12-06 23:22:27 +0500

    fix lad_13 click on TextBlock

--- lab_13/Task_1/Task_1/MainWindow.xaml.cs
+++ lab_13/Task_1/Task_1/MainWindow.xaml.cs
@@ -14,15 +14,76 @@ namespace Task_1
             InitializeComponent();
         }
 
-        private void lbl1_MouseDown(object sender, MouseButtonEventArgs e)
+        private bool _afterPutIntTextBox;
+        private UIElement _dragSource;
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Label lbl = (Label)sender;
-            DragDrop.DoDragDrop(lbl, lbl.Content, DragDropEffects.Copy);
+            _dragSource = lbl;
+            DragDrop.DoDragDrop(lbl, lbl, DragDropEffects.Copy);
         }
 
-        private void txtTarget_Drop(object sender, DragEventArgs e)
+        private void Grid_OnDrop(object sender, DragEventArgs e)
         {
-            ((TextBlock)sender).Text = (string)e.Data.GetData(DataFormats.Text);
+            var lbl = (Label)e.Data.GetData(typeof(Label));
+            if (!_afterPutIntTextBox)
+            {
+                if (Grid.Children.Contains(lbl))
+                {
+                    lbl.Margin = new Thickness(e.GetPosition(this).X - lbl.ActualWidth / 2,
+                        e.GetPosition(this).Y - lbl.ActualHeight / 2, 0, 0);
+                }
+                else
+                {
+                    lbl.Margin = new Thickness(e.GetPosition(this).X - lbl.Width / 2,
+                        e.GetPosition(this).Y - lbl.Height / 2, 0, 0);
+                    Grid.Children.Add(lbl);
+                    if (_dragSource.GetType() == typeof(TextBlock))
+                    {
+                        var tb1 = (TextBlock)_dragSource;
+                        tb1.Text = "";
+                        tb1.AllowDrop = true;
+                    }
+                }
+            }
+            else
+            {
+                _afterPutIntTextBox = false;
+            }
+        }
+
+        private void UIElement_OnDrop(object sender, DragEventArgs e)
+        {
+            var tb = (TextBlock)sender;
+            var lbl = (Label)e.Data.GetData(typeof(Label));
+            tb.Text = lbl.Content.ToString();
+            Grid.Children.Remove(lbl);
+            _afterPutIntTextBox = true;
+            tb.AllowDrop = false;
+            if (_dragSource.GetType() == typeof(TextBlock))
+            {
+                var tb1 = (TextBlock)_dragSource;
+                tb1.Text = "";
+                tb1.AllowDrop = true;
+            }
+        }
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var tb = (TextBlock)sender;
+            var lbl = new Label();
+            lbl.Content = tb.Text;
+            lbl.Height = 30;
+            lbl.Width = 60;
+            lbl.VerticalAlignment = VerticalAlignment.Top;
+            lbl.HorizontalAlignment = HorizontalAlignment.Left;
+            lbl.AllowDrop = false;
+            lbl.MouseDown += (o, args) =>
+            {
+                Label_MouseDown(lbl, e);
+            };
+            _dragSource = tb;
+            DragDrop.DoDragDrop(tb, lbl, DragDropEffects.Copy);
         }
     }
 }
---
+++ lab_13/Task_2/Task_2/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlockColor_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        private void ListBox1_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
