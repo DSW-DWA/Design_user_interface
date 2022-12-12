a5e8a729ee910cec993966647fac89c47d0693d6
Andrei Chupeev <cad.dsw.dwa@gmail.com>
2022-12-12 19:23:25 +0500

    add lab_13 task 2

--- lab_13/Task_2/Task_2/MainWindow.xaml.cs
+++ lab_13/Task_2/Task_2/MainWindow.xaml.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using Microsoft.SqlServer.Server;
 
 namespace Task_2
 {
@@ -21,6 +19,8 @@ namespace Task_2
             InitializeComponent();
         }
 
+        private Brush _listBox1Brush = Brushes.Black;
+        private Brush _listBox2Brush = Brushes.Black;
         private UIElement _dragSource;
         private bool _isLeftMouse = true;
 
@@ -47,6 +47,7 @@ namespace Task_2
                             {
                                 var lbInsert = (ListBoxItem)lbSelectedItem;
                                 var item = new ListBoxItem();
+                                item.Foreground = _listBox1Brush;
                                 item.Content = lbInsert.Content;
                                 ListBox1.Items.Add(item);
                             }
@@ -98,14 +99,29 @@ namespace Task_2
                     var lb = (ListBox)data;
                     if (lb != lbMain)
                     {
+                        var del = new List<ListBoxItem>();
                         foreach (var lbSelectedItem in lb.SelectedItems)
                         {
                             var lbInsert = (ListBoxItem)lbSelectedItem;
                             var item = new ListBoxItem();
+                            if (lbMain == ListBox1)
+                            {
+                                item.Foreground = _listBox1Brush;
+                            }
+                            if (lbMain == ListBox2)
+                            {
+                                item.Foreground = _listBox2Brush;
+                            }
                             item.Content = lbInsert.Content;
                             lbMain.Items.Add(item);
+                            del.Add(lbInsert);
                         }
-                        TextBlockInfo.Text = $"{lb.SelectedItems.Count} позиций персено из {lb.Name} в {lbMain.Name}";
+
+                        foreach (var lbSelectedItem in del)
+                        {
+                            lb.Items.Remove(lbSelectedItem);
+                        }
+                        TextBlockInfo.Text = $"{del.Count} позиций персено из {lb.Name} в {lbMain.Name}";
                     }
                 }
             }
@@ -118,6 +134,14 @@ namespace Task_2
                     {
                         var lbi = new ListBoxItem();
                         lbi.Content = (string)data;
+                        if (lbMain == ListBox1)
+                        {
+                            lbi.Foreground = _listBox1Brush;
+                        }
+                        if (lbMain == ListBox2)
+                        {
+                            lbi.Foreground = _listBox2Brush;
+                        }
                         lbMain.Items.Insert(0,lbi);
                         TextBlockInfo.Text = $"Позиция перенесена в начало {lbMain.Name}";
                     }
@@ -125,6 +149,14 @@ namespace Task_2
                     {
                         var lbi = new ListBoxItem();
                         lbi.Content = (string)data;
+                        if (lbMain == ListBox1)
+                        {
+                            lbi.Foreground = _listBox1Brush;
+                        }
+                        if (lbMain == ListBox2)
+                        {
+                            lbi.Foreground = _listBox2Brush;
+                        }
                         lbMain.Items.Add(lbi);
                         TextBlockInfo.Text = $"Позиция перенесена в конец {lbMain.Name}";
                     }
@@ -141,6 +173,7 @@ namespace Task_2
                     var b = (byte)(255*(point.X / 200));
                     var brush = new SolidColorBrush(Color.FromRgb(r,0,b));
                     lbMain.Background = brush;
+                    ColorRectangle.Fill = brush;
                 }
                 TextBlockInfo.Text = $"Фон {lbMain.Name} изменил цвет";
             }
@@ -272,6 +305,15 @@ namespace Task_2
                         var lbi = (ListBoxItem)lbItem;
                         lbi.Foreground = brush;
                     }
+                    if (lb == ListBox1)
+                    {
+                        _listBox1Brush = brush;
+                    }
+                    if (lb == ListBox2)
+                    {
+                        _listBox2Brush = brush;
+                    }
+                    ColorRectangle.Fill = brush;
                     TextBlockInfo.Text = $"Шрифт {lb.Name} изменил цвет";
                 }
             }
